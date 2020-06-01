e17d068bd916b34e71055fefc1bee68a6da667ac
ujjwal <ujjwalmaity98@gmail.com>
2020-06-02 02:22:50 +0530

    Read SMS & Show SMS

--- app/src/main/java/dev/ujjwal/transactionalsmsanalyzer/view/activity/MainActivity.kt
+++ app/src/main/java/dev/ujjwal/transactionalsmsanalyzer/view/activity/MainActivity.kt
@@ -1,7 +1,14 @@
 package dev.ujjwal.transactionalsmsanalyzer.view.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import dev.ujjwal.transactionalsmsanalyzer.R
 import dev.ujjwal.transactionalsmsanalyzer.model.SMS
@@ -10,22 +17,66 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+        private const val MY_PERMISSIONS_REQUEST_READ_SMS = 1
+    }
+
+    private val smsList = ArrayList<SMS>()
     private val smsListAdapter = SmsListAdapter(this, arrayListOf())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        checkPermission()
+
         recycler_view.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = smsListAdapter
         }
 
-        val userList = ArrayList<SMS>()
-        userList.add(SMS('a'.toString()))
-        userList.add(SMS('b'.toString()))
-        userList.add(SMS('c'.toString()))
-        userList.add(SMS('d'.toString()))
-        smsListAdapter.updateSms(userList)
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) == PackageManager.PERMISSION_GRANTED) {
+            refreshSmsInbox()
+        }
+    }
+
+    private fun checkPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_SMS), MY_PERMISSIONS_REQUEST_READ_SMS)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_READ_SMS -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    Log.i(TAG, "Permission was granted for Read SMS")
+                    refreshSmsInbox()
+                } else {
+                    Log.i(TAG, "Permission was denied for Read SMS")
+                    Toast.makeText(applicationContext, "Permission was denied for Read SMS", Toast.LENGTH_LONG).show()
+                }
+            }
+
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+    }
+
+    private fun refreshSmsInbox() {
+        val contentResolver = contentResolver
+        val smsInboxCursor: Cursor? = contentResolver.query(Uri.parse("content://sms/inbox"), null, null, null, null)
+        val indexBody: Int = smsInboxCursor!!.getColumnIndex("body")
+        val indexAddress: Int = smsInboxCursor.getColumnIndex("address")
+        if (indexBody < 0 || !smsInboxCursor.moveToFirst()) return
+        smsList.clear()
+        do {
+            val sender = smsInboxCursor.getString(indexAddress).trimIndent()
+            val text = smsInboxCursor.getString(indexBody).trimIndent()
+            smsList.add(SMS(sender + "\n\n" + text))
+        } while (smsInboxCursor.moveToNext())
+        smsListAdapter.updateSms(smsList)
     }
 }
